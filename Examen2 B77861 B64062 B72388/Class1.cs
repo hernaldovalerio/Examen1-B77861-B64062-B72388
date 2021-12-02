aaac6dbdf246142d538aaaa3140f816d35fd8c9d
mairenaRo <40721741+mairenaRo@users.noreply.github.com>
2021-12-01 18:48:28 -0600

    Creación del proyecto de clases

---
+++ Examen2 B77861 B64062 B72388/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen2_B77861_B64062_B72388
+{
+    public class Class1
+    {
+    }
+}
