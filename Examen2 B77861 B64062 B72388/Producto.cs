130c9c91774de7ccb99edddbd537f3538e2226be
mairenaRo <40721741+mairenaRo@users.noreply.github.com>
2021-12-01 20:04:35 -0600

    Se realizan los cambios para la liberación del codigo a master

--- Examen2 B77861 B64062 B72388/Producto.cs
+++ Examen2 B77861 B64062 B72388/Producto.cs
@@ -4,19 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-public class Producto
+namespace Examen2_B77861_B64062_B72388
 {
-	public Producto()
+	public class Producto
 	{
+		public Producto()
+		{
+		}
+
+		public Producto(string descripcion_producto, string preparacion_producto)
+		{
+			this.descripcion_producto = descripcion_producto;
+			this.preparacion_producto = preparacion_producto;
+		}
+
+		string descripcion_producto { get; set; }
+		string preparacion_producto { get; set; }
+
 	}
-
-	public Producto(string descripcion_producto, string preparacion_producto)
-	{
-		this.descripcion_producto = descripcion_producto;
-		this.preparacion_producto = preparacion_producto;
-	}
-
-	string descripcion_producto { get; set; }
-	string preparacion_producto { get; set; }
-
 }
+
