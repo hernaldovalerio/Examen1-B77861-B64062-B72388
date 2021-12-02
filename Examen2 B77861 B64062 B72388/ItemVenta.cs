9b890eae6a472c077168776979af07323542d72c
mairenaRo <40721741+mairenaRo@users.noreply.github.com>
2021-12-01 19:13:55 -0600

    Creción de clases solicitadas.

---
+++ Examen2 B77861 B64062 B72388/ItemVenta.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class ItemVenta
+{
+	public ItemVenta()
+	{
+	}
+
+	public ItemVenta(int Cantidad_Vendida, float precio_unitario, int numero_venta, int producto_id)
+	{
+		this.Cantidad_Vendida = Cantidad_Vendida;
+		this.precio_unitario = precio_unitario;
+		this.numero_venta = numero_venta;
+		this.producto_id = producto_id;
+
+	int Cantidad_Vendida { get; set; }
+	float precio_unitario { get; set; }
+	int numero_venta { get; set; }
+	int producto_id { get; set; }
+
+}
