f74d04c71202cf656dc18ab2011eebb30c38b6f8
mairenaRo <40721741+mairenaRo@users.noreply.github.com>
2021-12-01 19:38:20 -0600

    Se agrega la clase VentaData al desarrollo de la rama.

---
+++ Examen2 B77861 B64062 B72388/VentaData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class VentaData
+{
+	public VentaData()
+	{
+	}
+
+	public void insertar(Venta venta)
+    {
+
+    }
+
+}
